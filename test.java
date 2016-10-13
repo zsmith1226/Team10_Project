45ac2546c58ec510073ef651f4d346bad9ba6478
chris-lanini <chris.lanini@yahoo.com>
2016-10-12 22:22:57 -0700

    Added my name to the test.java file.

--- test.java
+++ test.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello this is Zuriel Smith");
+    	System.out.println("Hello, this is Chris Lanini");
     }
 
 }
