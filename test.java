2223091eb5f758f6ad6e80a0cc24aa74b1dda12f
dumblet <miah.jere18@gmail.com>
2016-10-12 23:56:41 -0700

    added hello, i'm jeremiah to test.java

--- test.java
+++ test.java
@@ -2,6 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello this is Zuriel Smith");
     	System.out.println("Hello, this is Chris Lanini");
+		System.out.println("Hello, this is Jeremiah Reyes");
     }
 
 }
